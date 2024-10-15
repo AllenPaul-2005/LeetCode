843ab7f5e9aadbdd770c5368949c889f65f6e643
Allen_Paul <163329337+AllenPaul-2005@users.noreply.github.com>
2024-10-15 21:35:07 +0530

    Time: 39 ms (21.05%), Space: 15.5 MB (89.89%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int whitePosition = 0;
-        long long totalSwaps = 0;
-
-        // Iterate through each ball (character) in the string
-        for (int currentPos = 0; currentPos < s.length(); currentPos++) {
-            if (s[currentPos] == '0') {
-                // Calculate the number of swaps needed
-                // to move it to the leftmost available position
-                totalSwaps += currentPos - whitePosition;
-
-                // Move the next available position for a white ball one step to
-                // the right
-                whitePosition++;
+        long long res=0;
+        int l=0;
+        for(int j=0;j<s.size();j++){
+            if(s[j]=='0'){
+                res+=(j-l);
+                l+=1;
             }
         }
-
-        return totalSwaps;
+        return res;
+        
     }
 };
