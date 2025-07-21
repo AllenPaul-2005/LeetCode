402f3a12f6b863c33a58abd2a839f8af7f6807c5
Allen_Paul <163329337+AllenPaul-2005@users.noreply.github.com>
2025-07-21 14:37:25 +0530

    Time: 21 ms (60.58%), Space: 19.9 MB (26.59%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -3,11 +3,11 @@ class Solution:
 
         g.sort()
         s.sort()
-        count = 0
+       
         i, j = 0, 0
         while i<len(g) and j <len(s):
             if s[j]>=g[i]:
-                count+=1
+                
                 i+=1
             j+=1
         return i
